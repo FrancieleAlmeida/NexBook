a4be3fd9d96d77e769a7e0f80441b0724b57db53
FrancieleAlmeida <francielytlc3@gmail.com>
2025-06-20 20:40:05 -0400

    iniciando o react native com navegação em abas

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="search" options={{ title: 'Search' }} />
+      <Tabs.Screen name="fav" options={{ title: 'Fav' }} />
+      <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+    </Tabs>
+  );
+}
---
+++ src/app/(tabs)/fav.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>favoritos NexBook</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
