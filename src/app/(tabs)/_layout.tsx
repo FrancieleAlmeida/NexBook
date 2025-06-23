b6a10b7e34899616d457fa53a9825bd95b50c2c5
IsabelleRancan <isabellerancan@gmail.com>
2025-06-23 16:51:59 -0400

    Criando a tela de Home
    Tela de home criada como base para consumir a API;
    Definição dos ícones de cada uma das Tabs.

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,12 +1,13 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="search" options={{ title: 'Search' }} />
-      <Tabs.Screen name="fav" options={{ title: 'Fav' }} />
-      <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({color, size}) => ( <Ionicons name="book-outline" color={color} size={size} />) }} />
+      <Tabs.Screen name="search" options={{ title: 'Search', tabBarIcon: ({ color, size }) => ( <Ionicons name="search-outline" color={color} size={size} /> ) }} />
+      <Tabs.Screen name="fav" options={{ title: 'Fav', tabBarIcon: ({color, size}) => ( <Ionicons name="bookmarks-outline" color={color} size={size}  /> )}} />
+      <Tabs.Screen name="profile" options={{ title: 'Profile', tabBarIcon: ({ color, size }) => ( <Ionicons name="person-outline" color={color} size={size} /> ) }} /> 
     </Tabs>
   );
 }
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,17 +1,37 @@
-import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import Carousel from '@/components/Carrousel';
 
-export default function AboutScreen() {
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Inicio NexBook</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.appName}>NexBook</Text>
+
+      <Text style={styles.sectionTitle}>Novos</Text>
+      <Carousel type="novos" />
+
+      <Text style={styles.sectionTitle}>Populares</Text>
+      <Carousel type="populares" />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-})
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 16,
+  },
+  appName: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 24,
+    marginBottom: 12,
+  },
+});
